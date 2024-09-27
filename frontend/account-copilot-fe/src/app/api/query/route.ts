01799bb8d64657f5708df8d1cb27f7da9684aeb9
Bhaulik Patel <bhaulikpatel966@gmail.com>
2024-09-27 16:07:03 -0500

    updated requirement and backend query

--- frontend/account-copilot-fe/src/app/api/query/route.ts
+++ frontend/account-copilot-fe/src/app/api/query/route.ts
@@ -4,7 +4,7 @@ export async function POST(request: Request) {
   try {
     const body = await request.json();
     const backendUrl = process.env.BACK_END_KEY || "http://0.0.0.0:8000";
-    const response = await fetch(`${backendUrl}`, {
+    const response = await fetch(`${backendUrl}/query`, {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
